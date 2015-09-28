f377fd3b8d3b3907c5388f63f80786da5156583c
Anthony Jee <anthonyjee0512@gmail.com>
2015-09-28 21:45:50 +0800

    bug fix 6

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :check_user, except: [:index, :show]
+  # before_action :check_user, except: [:index, :show]
   # GET /restaurants
   # GET /restaurants.json
 
